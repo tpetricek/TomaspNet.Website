e5f97fbb73e7e7210868d09e11495bb6c06fbe50
Tomas Petricek <tomas@tomasp.net>
2013-10-14 04:32:15 -0400

    Add Lanyrd news

--- content/news/news.js
+++ content/news/news.js
@@ -1,5 +1,7 @@
-var news =
-{ "date": "September 2013, NYC",
+// Explicit 
+var news1 =
+{
+  "date": "September 2013, NYC",
   "info": "\
  \
   <p style='text-align:center'>\
@@ -11,4 +13,19 @@ var news =
   Become F# master in <strong>four days</strong> in <strong>New York</strong>! <br />If you come to \
   our two day training on September 16-17, you get a free ticket to Progressive F# \
   tutorials on September 18-19. If you're interested <a href='mailto:tomas@tomasp.net'>email me</a>! \
-  </p>" }
+  </p>"
+}
+
+// Lanyrd 
+var news =
+  {
+    "date": "Talks in Autumn 2013",
+    "info": '\
+     <p>See my <a href="http://lanyrd.com/profile/tomasp/">Lanyrd page</a> for more talks and events. \
+        Here are the closest events where you can meet me:</p>\
+     <div class="lanyrd-target-splat"> \
+       <a href="http://lanyrd.com/profile/tomasp/" class="lanyrd-splat lanyrd-template-standard lanyrd-number-3 lanyrd-nolink lanyrd-nostyles lanyrd-headingstart-h5" rel="me">\
+         See my conferences on Lanyrd\
+       </a>\
+     </div>'
+  }

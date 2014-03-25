1ddeabb4cb80e1a546389a0b00603651d26a2746
Tomas Petricek <tomas@tomasp.net>
2014-03-25 14:35:06 +0000

    Add puzzle blog and work in progress update monads

---
+++ source/blog/2014/abstracts/puzzling-fsharp.fsx
@@ -0,0 +1,19 @@
+﻿(**
+Do you need to convince your friends & family that programming can be fun? 
+For the last Christmas, I got a puzzle as a gift. It is a number of small
+cubes, joined together, that can be rotated and folded to form a bigger
+(4x4x4) cube. 
+
+We spent the last few days of the year with family and a couple of friends
+and I left the puzzle on the table. Every time I walked around, someone was
+playing with it without much success. In the end, I said that if noone solves
+it until 31 December, I'll write a program to do it. Which I did between 7 PM
+and 8 PM and, voilà, here is what I got...
+
+<div style="text-align:center;margin:10px">
+<a href="http://tomasp.net/blog/2014/puzzling-fsharp/puzzle.jpg" target="_blank"><img src="http://tomasp.net/blog/2014/puzzling-fsharp/puzzle.jpg" style="height:200px;margin-right:20px;border-style:none" /></a>
+<a href="http://tomasp.net/blog/2014/puzzling-fsharp/cube.jpg" target="_blank"><img src="http://tomasp.net/blog/2014/puzzling-fsharp/cube.jpg" style="height:200px;margin-left:20px;border-style:none" /></a>
+</div>
+
+So, how do you solve a puzzle in about 1 hour on New Year's eve?
+*)

c3ea59d53813f11394f700e094c270579dbdb008
Tomas Petricek <tomas@tomasp.net>
2014-12-08 17:31:14 +0000

    Adding #FsAdvent blog post

---
+++ source/blog/2014/abstracts/composing-christmas.fsx
@@ -0,0 +1,19 @@
+﻿(**
+<img src="http://tomasp.net/blog/2014/composing-christmas/glowing.gif" style="float:right;margin:20px; width:200px" /> 
+
+This blog post is a part of the awesome [F# Advent Calendar](https://sergeytihon.wordpress.com/2014/11/24/f-advent-calendar-in-english-2014/)
+(see the previous entry about [writing algorithms in F#](http://richardminerich.com/2014/12/developing-an-algorithm-in-f-fast-rotational-alignments-with-gospers-hack/)
+from Rick Minerich), so it inevitably needs a Christmassy theme. However, there is also 
+going to be a serious theme for the blog post, which is domain-specific languages.
+
+One of my favorite examples of Domain-Specific Languages is a simple OpenGL library
+that I wrote some time ago for composing 3D graphics in F#. You can see it in my 
+NDC 2014 talk [Domain Specific Languages, the functional way](http://vimeo.com/97315970)
+and I also used it for [Solving Puzzles with F#](http://tomasp.net/blog/2014/puzzling-fsharp/index.html)
+earlier on this blog. 
+
+The nice thing about the library is that it is very simple, but is rich enough to demonstrate
+all the important concepts. In fact, the library is so easy to use that [even 8 years old can
+do a talk about it](https://twitter.com/ptrelford/status/538098139430137856). So, if you're
+spending Christmas with your family, perhaps you can go through this article with your children!
+*)
